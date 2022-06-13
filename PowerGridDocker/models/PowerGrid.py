a51b78f89d716a3298b1fb0aaddff97cfc0c038a
Krystian Bezpalko <83120622+Krystian-P@users.noreply.github.com>
2022-06-14 00:25:14 +0200

    Version 1.0

--- PowerGridDocker/models/PowerGrid.py
+++ PowerGridDocker/models/PowerGrid.py
@@ -49,15 +49,30 @@ class PowerGrid:
         graphNodesList = [(str(node.nod_id), str("{:.0f}".format(node.nod_id)), str(node.positivCheck())) for node in
                           self.nodes]
         for gen in self.gens:
-            if self.nodes[int(gen.nod_id)-1].balance >= 0:
-                graphNodesList[int(gen.nod_id)-1] = (str(gen.nod_id), str("{:.0f}".format(gen.nod_id)), 'generatorP')
+            if self.nodes[int(gen.nod_id) - 1].balance >= 0:
+                graphNodesList[int(gen.nod_id) - 1] = (str(gen.nod_id), str("{:.0f}".format(gen.nod_id)), 'generatorP')
             else:
                 graphNodesList[int(gen.nod_id) - 1] = (str(gen.nod_id), str("{:.0f}".format(gen.nod_id)), 'generatorN')
-        return graphNodesList
+        nodes = [
+            {
+                'data': {'id': short, 'label': label},
+                'grabbable': False,
+                'classes': color
+            }
+            for short, label, color in (
+                graphNodesList
+            )
+        ]
+        return nodes
 
     def prepNodeDataFrame(self):
         nodeDataFrame = [(str(node.nod_id), str("{:.2f}".format(node.balance))) for node in self.nodes]
-        return nodeDataFrame
+        nodeColumns = [{'id': 'Node number', 'name': 'Node number'}]
+        nodeRows = {'Node number': 'Node power balance [MW]'}
+        for col, value in nodeDataFrame:
+            nodeColumns.append({'id': col, 'name': col})
+            nodeRows[col] = value
+        return nodeColumns, nodeRows
 
     def prepBranches(self):
         if self.nClusters == 1:
@@ -66,12 +81,29 @@ class PowerGrid:
         else:
             graphBranchesList = [(str(branch.node_from), str(branch.node_to), str("{:.2f}".format(branch.flow)),
                                   str(self.colorList[int(branch.cluster)])) for branch in self.branches]
-        return graphBranchesList
+        edges = [
+            {'data':
+                 {'id': source + target,
+                      'source': source, 'target': target,
+                      'label': label,
+                      'color': color}}
+
+            for source, target, label, color in (
+                graphBranchesList
+            )
+        ]
+        return edges
 
     def genrationPlotGenerators(self):
         dataFrameGeneratorsList = [[str(gen.nod_id), "{:.2f}".format(gen.generation)] for gen
                                    in self.gens]
-        return dataFrameGeneratorsList
+        genColumns = [{'id': 'Location of Generator', 'name': 'Location of Generator'}]
+        genRows = {'Location of Generator': 'Power Generating'}
+        for col, value in dataFrameGeneratorsList:
+            genColumns.append({'id': col, 'name': col})
+            genRows[col] = value
+
+        return genColumns, genRows
 
     def getNodeInfo(self, nodeId):
         node = self.nodes[int(nodeId) - 1]
@@ -80,9 +112,11 @@ class PowerGrid:
             "Node Type": node.node_type,
             'Node Demand': "{:.2f}".format(node.demand)
         }
-        return noteDict
+        dataRows = [{'Node Id': noteDict['Node Id'], 'Node Type': noteDict["Node Type"],
+                     'Node Demand': noteDict['Node Demand']}]
+        return dataRows
 
-    def clustersDataFrame(self):
+    def clustersDataFrame(self, colorsList):
         dataFrameBranchesList = [[branch.cluster, branch.flow] for branch in self.branches]
         dataFrame = pd.DataFrame(dataFrameBranchesList, columns=['cluster', 'flow'])
         groupedData = dataFrame.groupby('cluster')
@@ -90,4 +124,11 @@ class PowerGrid:
         minimums = groupedData.min()
         clusterData = minimums.merge(maximums, how='left', on='cluster')
 
-        return clusterData.sort_values(by=['flow_x'])
+        clusterRows = []
+        style_data_conditional = []
+        for row in clusterData.iterrows():
+            clusterDict = {'Cluster number': row[0], 'Max': "{:.2f}".format(row[1][1]),
+                           'Min': "{:.2f}".format(row[1][0])}
+            clusterRows.append(clusterDict)
+            style_data_conditional.append({'if': {'row_index': row[0]}, 'backgroundColor': colorsList[int(row[0])]})
+        return clusterRows, style_data_conditional
